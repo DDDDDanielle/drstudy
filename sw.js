2152dffa7a75717b8a930aca4bdd571f2cccb4a9
DDDDDanielle <56327112+DDDDDanielle@users.noreply.github.com>
2026-08-25 14:39:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'drstudy-v8';
+const CACHE = 'drstudy-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon-180.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
